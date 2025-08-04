e6cd46a695fe9b59a05647fb9e900cc873535753
Kauan Barbosa <kauan.barbosa.santos07@escola.pr.gov.br>
2025-08-04 08:36:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function mostrarQuiz(disciplina) {
+  switch (disciplina) {
+    case 'portugues':
+      alert("Quiz: Qual é a ideia central do texto apresentado?");
+      break;
+    case 'matematica':
+      alert("Desafio: Resolva 3 problemas com números racionais!");
+      break;
+    case 'historia':
+      alert("Desafio: Qual foi a principal causa da Revolução Industrial?");
+      break;
+  }
+}
